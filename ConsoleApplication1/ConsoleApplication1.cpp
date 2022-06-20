057f0944cac8e4aa7df1b83d96a75225c03d8877
DILAN0 <artembelykov04@gmail.com>
2022-06-20 22:35:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL,"RUS");
+	double a, b, c, V, S;
+	cout << "Введите a,b,c: ";
+	cin >> a >> b >> c;
+	V = a * b * c;
+	S = 2 * (a * b + b * c + a * c);
+	cout << "Объем: " << V << " " << "Площадь: " << S;
+}
